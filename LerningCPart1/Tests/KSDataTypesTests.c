560aeb6169deb6db02ee49abf22ca5a3eeebd2ec
Serg Bla <Serhii@MacBook-Air-Serg.local>
2015-09-21 19:34:46 +0300

    no message

---
+++ LerningCPart1/Tests/KSDataTypesTests.c
@@ -0,0 +1,13 @@
+//
+//  DataTypesTests.c
+//  LerningCPart1
+//
+//  Created by Serg Bla on 18.09.15.
+//  Copyright © 2015 Serg Bla. All rights reserved.
+//
+
+#include "DataTypesTests.h"
+KSSizeOfDataTypes (char);
+KSSizeOfDataTypes (int);
+KSSizeOfDataTypes (float);
+KSSizeOfDataTypes (double);
---
+++ LerningCPart1/Tests/KSDataTypesTests.h
@@ -0,0 +1,15 @@
+//
+//  DataTypesTests.h
+//  LerningCPart1
+//
+//  Created by Serg Bla on 18.09.15.
+//  Copyright © 2015 Serg Bla. All rights reserved.
+//
+
+#ifndef DataTypesTests_h
+#define DataTypesTests_h
+
+extern
+void KSPrintSizeOfDataTypes(void);
+
+#endif /* DataTypesTests_h */
