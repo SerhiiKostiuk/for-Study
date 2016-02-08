7ddbf54514bb2c023dfbcad11e71d2d81039240b
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-02-08 11:23:25 +0200

    refactor CGGeometry+KSExtensions.h

--- ProjectUI/Source/Library/Categories/CGGeometry/CGGeometry+KSExtensions.h
+++ ProjectUI/Source/Library/Categories/CGGeometry/CGGeometry+KSExtensions.h
@@ -8,28 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-/* Return the width of `frame'. */
+// Return the width of `frame'.
 #define CGWidth(frame) CGRectGetWidth(frame)
 
-/* Return the height of `frame'. */
+// Return the height of `frame'.
 #define CGHeight(frame) CGRectGetHeight(frame)
 
-/* Return the leftmost x-value of `frame'. */
+// Return the leftmost x-value of `frame'.
 #define CGMinX(frame) CGRectGetMinX(frame)
 
-/* Return the midpoint x-value of `frame'. */
+// Return the midpoint x-value of `frame'.
 #define CGMidX(frame) CGRectGetMidX(frame)
 
-/* Return the rightmost x-value of `frame'. */
+// Return the rightmost x-value of `frame'.
 #define CGMaxX(frame) CGRectGetMaxX(frame)
 
-/* Return the bottommost y-value of `frame'. */
+// Return the bottommost y-value of `frame'.
 #define CGMinY(frame) CGRectGetMinY(frame)
 
-/* Return the midpoint y-value of `frame'. */
+// Return the midpoint y-value of `frame'.
 #define CGMidY(frame) CGRectGetMidY(frame)
 
-/* Return the topmost y-value of `frame'. */
+// Return the topmost y-value of `frame'.
 #define CGMaxY(frame) CGRectGetMaxY(frame)
 
 extern
