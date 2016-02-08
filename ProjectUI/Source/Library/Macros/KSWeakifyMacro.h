c100b89826644e5536f413e89e3d11fcd3bed33a
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-02-08 11:24:03 +0200

    refactor KSClangDiagnosticPushOption

--- ProjectUI/Source/Library/Macros/KSWeakifyMacro.h
+++ ProjectUI/Source/Library/Macros/KSWeakifyMacro.h
@@ -8,18 +8,19 @@
 
 #define KSClangDiagnosticPush    _Pragma ("clang diagnostic push")
 #define KSClangDiagnosticPop     _Pragma ("clang diagnostic pop")
-#define KSClangDiagnosticIgnored _Pragma ("clang diagnostic ignored \"-Wshadow\"")
 
-#define KSClangDiagnosticPushIgnored \
+#define KSClangDiagnosticPushOption(option) \
     KSClangDiagnosticPush \
-    KSClangDiagnosticIgnored \
+    _Pragma(option) \
+
+#define KSClangDiagnosticPopOption KSClangDiagnosticPop
 
 #define KSWeakify(obj) __weak typeof(obj) __weak_##obj = obj
 
 #define KSStrongify(obj) \
-    KSClangDiagnosticPushIgnored \
+    KSClangDiagnosticPushOption("clang diagnostic ignored \"-Wshadow\"") \
     __strong typeof(__weak_##obj) obj = __weak_##obj; \
-    KSClangDiagnosticPop
+    KSClangDiagnosticPopOption
 
 #define KSStrongifyAndReturnValueIfNil(obj, value) \
     KSStrongify(obj); \
