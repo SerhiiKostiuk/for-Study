133a6fcc0bc227e4921af2992c1740cb50e38286
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-02-08 13:11:15 +0200

    add KSObservableObject

---
+++ ProjectUI/Source/Library/KSObservableObject/KSObservableObject.h
@@ -0,0 +1,28 @@
+//
+//  KSObjservableObject.h
+//  LearningDevelop
+//
+//  Created by Serg Bla on 20.12.15.
+//  Copyright © 2015 Serg Bla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSObservableObject : NSObject
+@property (nonatomic, readonly) NSSet      *observers;
+@property (nonatomic, assign)   NSUInteger state;
+
+- (void)setState:(NSUInteger)state withObject:(id)object;
+
+- (void)addObserver:(id)observer;
+- (void)addObserversFromArray:(NSArray *)observers;
+
+- (void)removeObserver:(id)observer;
+- (void)removeObserversFromArray:(NSArray *)observers;
+
+- (void)notifyObserversWithSelector:(SEL)selector;
+- (void)notifyObserversWithSelector:(SEL)selector withObject:(id)object;
+
+- (SEL)selectorForState:(NSUInteger)state;
+
+@end
---
+++ ProjectUI/Source/Library/KSObservableObject/KSReference/KSReference.m
@@ -0,0 +1,49 @@
+//
+//  KSReference.m
+//  LearningDevelop
+//
+//  Created by Serg Bla on 20.12.15.
+//  Copyright © 2015 Serg Bla. All rights reserved.
+//
+
+#import "KSReference.h"
+
+@implementation KSReference
+@dynamic target;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.target = nil;
+    
+}
+
+- (instancetype)initWithTarget:(id)target {
+    self = [super init];
+    if (self) {
+        self.target = target;
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark NSCopying
+
+- (id)copyWithZone {
+    return [[[self class] alloc] initWithTarget:self.target];
+}
+
+#pragma mark -
+#pragma mark NSObject equlity
+
+- (NSUInteger)hash {
+    return [self.target hash];
+}
+
+- (BOOL)isEqual:(KSReference *)object {
+    return [object isMemberOfClass:[self class]] && object.target == self.target;
+}
+
+@end
