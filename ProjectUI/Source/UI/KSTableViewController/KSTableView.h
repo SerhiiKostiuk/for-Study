9695ea68a2e25923509508d4ca41ac11b840bc37
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-02-08 13:09:32 +0200

    add KSTableView

---
+++ ProjectUI/Source/UI/KSTableViewController/KSTableView.h
@@ -0,0 +1,13 @@
+//
+//  KSTableView.h
+//  KSProjectUI
+//
+//  Created by Serg Bla on 08.02.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSTableView : UIView
+
+@end
---
+++ ProjectUI/Source/UI/KSTableViewController/KSTableView.m
@@ -0,0 +1,21 @@
+//
+//  KSTableView.m
+//  KSProjectUI
+//
+//  Created by Serg Bla on 08.02.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSTableView.h"
+
+@implementation KSTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
