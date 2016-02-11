4bce01ada29ecce4061fcacd107925f62ad67015
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-02-11 17:00:49 +0200

    add KSUsers

--- ProjectUI/Source/Models/KSUser/KSUsers.h
+++ ProjectUI/Source/Models/KSUser/KSUsers.h
@@ -8,6 +8,18 @@
 
 #import "KSObservableObject.h"
 
+@class KSUser;
+
 @interface KSUsers : KSObservableObject
+@property (nonatomic, readonly) NSArray     *users;
+@property (nonatomic, readonly) NSUInteger  count;
+
+- (KSUser *)objectAtIndex:(NSUInteger)index;
+- (void)addObject:(KSUser *)object;
+- (void)insertObjectAtIndex:(KSUser *)object index:(NSUInteger)index;
+- (void)removeLastObject;
+- (void)removeObjectAtIndex:(NSUInteger)index;
+- (void)replaceObjectAtIndex:(NSUInteger)index withObject:(KSUser *)anObject;
+- (void)exchangeObjectAtIndex:(NSUInteger)firstIndex withObjectAtIndex:(NSUInteger)secondIndex;
 
 @end
--- ProjectUI/Source/Models/KSUser/KSUsers.m
+++ ProjectUI/Source/Models/KSUser/KSUsers.m
@@ -8,6 +8,108 @@
 
 #import "KSUsers.h"
 
+#import "KSUser.h"
+
+static const NSUInteger kKSUsersArraySize = 2;
+
+@interface KSUsers ()
+@property (nonatomic, strong) NSMutableArray *mutableUsers;
+
+- (void)fillWithUsers;
+
+@end
+
 @implementation KSUsers
+
+@dynamic users;
+@dynamic count;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self fillWithUsers];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSArray *)users {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        return [mutableUsers copy];
+    }
+}
+
+- (NSUInteger)count {
+    return self.mutableUsers.count;
+}
+
+#pragma mark -
+#pragma mark Public
+
+- (KSUser *)objectAtIndex:(NSUInteger)index {
+    return [self.mutableUsers objectAtIndex:index];
+}
+
+- (void)addObject:(KSUser *)object {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        [mutableUsers addObject:object];
+    }
+}
+- (void)insertObjectAtIndex:(KSUser *)object index:(NSUInteger)index {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        [mutableUsers insertObject:object atIndex:index];
+    }
+}
+
+- (void)removeLastObject {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        [mutableUsers removeLastObject];
+    }
+}
+
+- (void)removeObjectAtIndex:(NSUInteger)index {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        [mutableUsers removeObjectAtIndex:index];
+    }
+}
+
+- (void)replaceObjectAtIndex:(NSUInteger)index withObject:(KSUser *)object {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        [mutableUsers replaceObjectAtIndex:index withObject:object];
+    }
+}
+
+- (void)exchangeObjectAtIndex:(NSUInteger)firstIndex withObjectAtIndex:(NSUInteger)secondIndex {
+    NSMutableArray *mutableUsers = self.mutableUsers;
+    @synchronized(mutableUsers) {
+        [mutableUsers exchangeObjectAtIndex:firstIndex withObjectAtIndex:secondIndex];
+    }
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (void)fillWithUsers {
+    NSMutableArray *mutableUsers = [NSMutableArray arrayWithCapacity:kKSUsersArraySize];
+    @synchronized(mutableUsers) {
+        for (NSUInteger index = 0; index < kKSUsersArraySize; index++) {
+            [mutableUsers addObject:[KSUser new]];
+        }
+    }
+    
+    self.mutableUsers = mutableUsers;
+}
 
 @end
