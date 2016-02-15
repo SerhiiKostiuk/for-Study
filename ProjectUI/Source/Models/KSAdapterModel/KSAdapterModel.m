8ef4c1ed85cd16fe2671a8471d929518bb8595a2
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-02-15 17:01:48 +0200

    add KSAdapterModel, and  typedef for adapterState

---
+++ ProjectUI/Source/Models/KSAdapterModel/KSAdapterModel.m
@@ -0,0 +1,13 @@
+//
+//  KSAdapterModel.m
+//  KSProjectUI
+//
+//  Created by Serg Bla on 14.02.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSAdapterModel.h"
+
+@implementation KSAdapterModel
+
+@end
