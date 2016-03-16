347592c78c925f5d3fa90d00cabb2b8e086e8fef
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-03-16 18:49:25 +0200

    add Facebook idk from cocoapods

---
+++ ProjectUI/Source/UI/KSFacebookViewController/KSFacebookLoginViewController/KSFacebookLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSFacebookLoginViewController.h
+//  KSProjectUI
+//
+//  Created by Serg Bla on 16.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSFacebookLoginViewController : UIViewController
+
+@end
---
+++ ProjectUI/Source/UI/KSFacebookViewController/KSFacebookLoginViewController/KSFacebookLoginViewController.m
@@ -0,0 +1,44 @@
+//
+//  KSFacebookLoginViewController.m
+//  KSProjectUI
+//
+//  Created by Serg Bla on 16.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSFacebookLoginViewController.h"
+
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
+//#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface KSFacebookLoginViewController ()
+
+@end
+
+@implementation KSFacebookLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+//    loginButton.center = self.view.center;
+//    [self.view addSubview:loginButton];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
