c616369ae61456dc08d7491561fd1be07898a46a
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-03-21 14:59:08 +0200

    add base viewController

---
+++ ProjectUI/Source/Library/Classes/KSViewController/KSViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSViewController.h
+//  KSProjectUI
+//
+//  Created by Serg Bla on 20.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSViewController : UIViewController
+
+@end
