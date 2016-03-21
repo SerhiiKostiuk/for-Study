4b1636995db53c87160f67c74945bef2cfeb7b2e
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-03-21 14:51:17 +0200

    add KSArrayViewController

---
+++ ProjectUI/Source/Library/Classes/KSArrayViewController/KSArrayViewController.h
@@ -0,0 +1,17 @@
+//
+//  KSArrayViewController.h
+//  KSProjectUI
+//
+//  Created by Serg Bla on 20.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSViewController.h"
+
+@class KSArrayModel;
+
+@interface KSArrayViewController : KSViewController
+@property (nonatomic, strong) KSArrayModel *items;
+
+
+@end
---
+++ ProjectUI/Source/Library/Classes/KSArrayViewController/KSArrayViewController.m
@@ -0,0 +1,99 @@
+//
+//  KSArrayViewController.m
+//  KSProjectUI
+//
+//  Created by Serg Bla on 20.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSArrayViewController.h"
+
+#import "KSArrayModel.h"
+#import "KSUserCell.h"
+#import "KSCollectionObserver.h"
+
+#import "UITableView+KSExtensions.h"
+#import "UITableView+KSCollectionChangeModel.h"
+
+@interface KSArrayViewController () <UITableViewDelegate, UITableViewDataSource, KSCollectionObserver>
+
++ (Class)cellClass;
+- (id)cellClass;
+
+@end
+
+@implementation KSArrayViewController
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (Class)cellClass {
+    return nil;
+}
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.items = nil;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setItems:(KSArrayModel *)items {
+    if (_items != items) {
+        [_items removeObserver:self];
+        _items = items;
+        [_items addObserver:self];
+        
+        [_items load];
+        
+    }
+}
+
+#pragma mark -
+#pragma mark View Lifecycle
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.items load];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (id)cellClass {
+    return [[self class] cellClass];
+}
+
+- (UITableView *)tableView {
+    return nil;
+}
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    KSUserCell *cell = [tableView cellWithClass:[self cellClass]];
+    cell.user = self.items[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark -
+#pragma mark KSCollectionObserver
+
+- (void)collection:(id)collection didChangeWithModel:(id)changeModel {
+    UITableView *tableView = self.tableView;
+    [tableView updateWithCollectionChangeModel:changeModel];
+}
+
+@end
