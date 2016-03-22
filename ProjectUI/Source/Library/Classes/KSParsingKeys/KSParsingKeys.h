ce7980583ea4c1f0f3bb6ed1f7dd182e53fb7834
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-03-22 16:54:50 +0200

    add KSParsingKeys

---
+++ ProjectUI/Source/Library/Classes/KSParsingKeys/KSParsingKeys.h
@@ -0,0 +1,17 @@
+//
+//  KSParsingKeys.h
+//  KSProjectUI
+//
+//  Created by Serg Bla on 22.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString * const kFBError = @"error";
+static NSString * const kFBFirstNameKey = @"first_name";
+static NSString * const kFBLastNameKey = @"last_name";
+
+@interface KSParsingKeys : NSObject
+
+@end
---
+++ ProjectUI/Source/Library/Classes/KSParsingKeys/KSParsingKeys.m
@@ -0,0 +1,13 @@
+//
+//  KSParsingKeys.m
+//  KSProjectUI
+//
+//  Created by Serg Bla on 22.03.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSParsingKeys.h"
+
+@implementation KSParsingKeys
+
+@end
