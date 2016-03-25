a42adcf534c30550d5e73232b58e6be9bd0cf0e2
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-03-25 16:50:40 +0200

    remove macros

--- ProjectUI/Source/Library/Classes/KSModel/KSModel.h
+++ ProjectUI/Source/Library/Classes/KSModel/KSModel.h
@@ -8,28 +8,7 @@
 
 #import "KSObservableObject.h"
 
-#define KSModelPropertyDefine(modelClass, propertyName) \
-@property (nonatomic, strong) modelClass *propertyName;
 
-#define KSModelGetterDefine(modelClass, propertyName) \
-- (modelClass *)propertyName { \
-    return [self.model isKindOfClass:[modelClass class]] ? (modelClass *)self.model : nil; \
-}
-
-#define KSModelForModelPropertySyntesize(contextName, modelClass, propertyName) \
-\
-@interface contextName (_KSModelProperty__##contextName##__##modelClass##__##propertyName) \
-KSModelPropertyDefine(modelClass, propertyName) \
-\
-@end \
-\
-@implementation contextName (_KSModelProperty__##contextName##__##modelClass##__##propertyName) \
-\
-@dynamic propertyName; \
-\
-KSModelGetterDefine(modelClass, propertyName) \
-\
-@end
 
 typedef NS_ENUM(NSUInteger, KSModelState) {
     KSModelStateUnloaded,
