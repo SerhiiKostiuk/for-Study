748048100cc77294fb4eceb3df09257569a3a46b
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-04-12 20:34:59 +0300

    remove path and space between alloc and init

--- ProjectUI/Source/Contexts/KSFacebookLoginContext/KSFacebookLoginContext.m
+++ ProjectUI/Source/Contexts/KSFacebookLoginContext/KSFacebookLoginContext.m
@@ -21,20 +21,13 @@ KSModelForModelPropertySyntesize(KSFacebookLoginContext, KSUser, userModel);
 @implementation KSFacebookLoginContext
 
 #pragma mark -
-#pragma mark Public
-
-- (NSString *)path {
-    return kKSUserPath;
-}
-
-#pragma mark -
 #pragma mark Private
 
 - (void)load {
     KSWeakify(self);
     KSDispatchAsyncOnMainQueue(^{
         KSStrongifyAndReturnIfNil(self);
-        FBSDKLoginManager *login = [[FBSDKLoginManager alloc]init];
+        FBSDKLoginManager *login = [[FBSDKLoginManager alloc]   init];
         KSUser *user = self.userModel;
         [login logInWithReadPermissions:@[kKSUserEmailPermission, kKSPublicPermission, kKSUserFriendsPermission]
                      fromViewController:self.viewController
@@ -44,7 +37,7 @@ KSModelForModelPropertySyntesize(KSFacebookLoginContext, KSUser, userModel);
                                         NSLog(@"Process error");
                                         @synchronized(user) {
                                             user.state = KSModelStateFailedLoading;
-                                            }
+                                        }
                                     } else {
                                         FBSDKAccessToken *token = [FBSDKAccessToken currentAccessToken];
                                         user.ID = token.userID;
