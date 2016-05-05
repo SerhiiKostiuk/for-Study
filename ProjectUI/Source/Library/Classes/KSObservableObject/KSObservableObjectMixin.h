eb241312b6adb58b5d86bd8648f633d1a777ec55
Sergiy Kostiuk <k.seryoga@gmail.com>
2016-05-05 10:01:12 +0300

    add notification to coreData manager

---
+++ ProjectUI/Source/Library/Classes/KSObservableObject/KSObservableObjectMixin.h
@@ -0,0 +1,13 @@
+//
+//  KSObservableObjectMixin.h
+//  KSProjectUI
+//
+//  Created by Serg Bla on 04.05.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol KSObservableObjectMixin <NSObject>
+
+@end
